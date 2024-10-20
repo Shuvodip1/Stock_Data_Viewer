1129acea3f0100fa6cf5b1cd41a5972552178cee
Shuvodip1 <shuvodipduttabanik@gmail.com>
2024-10-20 13:23:57 +0530

    added environment variable

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 // Serve static files from the "public" directory
 app.use(express.static('public'));
